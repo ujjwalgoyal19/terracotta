775351757ea987bb07e73fbf180fdd204a31a52f
ujjwal-closeloop <ujjwal.goyal@closeloop.com>
2024-06-17 10:25:18 +0530

    Setup Eslint Configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,14 @@
-import sheriff from 'eslint-config-sheriff';
-import { defineFlatConfig } from 'eslint-define-config';
-import type { SheriffSettings } from '@sherifforg/types';
+import sheriff from "eslint-config-sheriff";
+import { defineFlatConfig } from "eslint-define-config";
+import type { SheriffSettings } from "@sherifforg/types";
 
 const sheriffOptions: SheriffSettings = {
-  "react": true,
-  "lodash": false,
-  "next": true,
-  "playwright": false,
-  "jest": false,
-  "vitest": false
+  react: true,
+  lodash: false,
+  next: true,
+  playwright: false,
+  jest: false,
+  vitest: false,
 };
 
 export default defineFlatConfig([...sheriff(sheriffOptions)]);
